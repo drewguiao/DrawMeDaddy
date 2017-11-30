b3c69bc78870a81c95e70733fbb3ba0e516de224
Justine Andrew Guiao <justine.guiao@orangeandbronze.com>
2017-11-30 10:16:46 +0800

    Added wordFetcher

---
+++ WordFetcher.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class WordFetcher {
+	private List<String> bagOfWords = new ArrayList<>();
+	private List<String> cache = new ArrayList<>();
+	private Random randomizer = new Random();
+	
+	public WordFetcher(){
+		try(BufferedReader breader = new BufferedReader(new FileReader("BagOfWords.txt"))){
+			while(breader.ready()){
+				String word = breader.readLine();
+				bagOfWords.add(word);
+			}
+		}catch(IOException ioe){
+			System.out.println("Error reading file!");
+		}
+	}
+	
+	public String fetch(){
+		int bagSize = bagOfWords.size();
+		int cacheSize = cache.size();
+		int index = randomizer.nextInt(bagSize);
+		
+		String word = bagOfWords.get(index);
+		
+		while(cache.contains(word)){
+			if(cacheSize == bagSize){
+				cache.clear();
+			}
+			index = randomizer.nextInt(bagSize);
+			word = bagOfWords.get(index);
+		}
+		
+		cache.add(word);
+		
+		return word;
+	}
